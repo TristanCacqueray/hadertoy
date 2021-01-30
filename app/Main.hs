ec16fe3621ad14cc2d5b1aef1f767331158ca070
Tristan Cacqueray <tdecacqu@redhat.com>
2021-01-30 17:07:43 +0000

    Integrate dear-imgui

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,73 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 -- | A demo hadertoy app
 module Main (main) where
 
+import Control.Exception (bracket, bracket_)
 import Control.Monad (void)
+import Control.Monad.Managed
 import Data.Maybe
+import DearImGui hiding (render)
+import qualified DearImGui
+import DearImGui.GLFW
+import DearImGui.GLFW.OpenGL
+import DearImGui.OpenGL
+import qualified Graphics.GL as GL
+import qualified Graphics.UI.GLFW as GLFW
 import Hadertoy
 import System.Environment (getArgs)
+
+withControllerWindow :: (GLFW.Window -> IO ()) -> IO ()
+withControllerWindow f = bracket mkWindow closeWindow runCallback
+  where
+    mkWindow :: IO (Maybe GLFW.Window)
+    mkWindow = GLFW.createWindow 800 600 "DearController" Nothing Nothing
+    closeWindow :: Maybe GLFW.Window -> IO ()
+    closeWindow = maybe (return ()) GLFW.destroyWindow
+    runCallback :: Maybe GLFW.Window -> IO ()
+    runCallback (Just win) = do
+      GLFW.makeContextCurrent (Just win)
+      GLFW.swapInterval 1
+      runManaged $ do
+        -- Create an ImGui context
+        _ <- managed $ bracket createContext destroyContext
+
+        -- Initialize ImGui's GLFW backend
+        _ <- managed_ $ bracket_ (glfwInitForOpenGL win True) glfwShutdown
+
+        -- Initialize ImGui's OpenGL backend
+        _ <- managed_ $ bracket_ openGL2Init openGL2Shutdown
+
+        liftIO $ f win
+
+      return ()
+    runCallback Nothing = error "GLFW createWindow failed"
+
+renderControllerWindow :: GLFW.Window -> IO ()
+renderControllerWindow win = do
+  -- Tell ImGui we're starting a new frame
+  GLFW.makeContextCurrent (Just win)
+  openGL2NewFrame
+  glfwNewFrame
+  newFrame
+
+  -- Build the GUI
+  bracket_ (begin "Hello, ImGui!") end do
+    -- Add a text widget
+    text "Hello, ImGui!"
+
+    -- Add a button widget, and call 'putStrLn' when it's clicked
+    button "Clickety Click" >>= \case
+      False -> return ()
+      True -> putStrLn "Ow!"
+
+  -- Render
+  GL.glClear GL.GL_COLOR_BUFFER_BIT
+  DearImGui.render
+  openGL2RenderDrawData =<< getDrawData
+  GLFW.swapBuffers win
 
 main :: IO ()
 main =
@@ -17,8 +78,9 @@ main =
     let version = fromMaybe V120 version'
     withGLFW version $ \glfw ->
       withWindow glfw 800 800 shader $ \win -> do
-        _ <- setEnvCenter win (-0.745, 0.0)
-        withMaybeJulia glfw fn shader win $ \juliaWin -> run fps (update glfw win juliaWin)
+        withControllerWindow $ \controllerWin -> do
+          _ <- setEnvCenter win (-0.745, 0.0)
+          withMaybeJulia glfw fn shader win $ \juliaWin -> run fps (update glfw win juliaWin controllerWin)
   where
     withMaybeJulia glfw fn shader mapWin f =
       if fn == "./shaders/mandelbrot.glsl"
@@ -37,7 +99,8 @@ main =
       void $ setEnvSeed mapWin coord
       void $ setEnvSeed juliaWin coord
     fps = 25
-    update glfw win juliaWin = do
+    update glfw win juliaWin controllerWin = do
+      renderControllerWindow controllerWin
       paused <- isPaused glfw
       if paused
         then return False
--- default.nix
+++ default.nix
@@ -1,10 +1,25 @@
+# Unfortunately, this doesn't work, nix-shell fails with:
+#
+# <command line>: cannot satisfy -package-id dear-imgui-1.0.0-inplace:
+#    dear-imgui-1.0.0-inplace is unusable due to missing dependencies:
+#      GLFW-b-3.3.0.0-LREaDWUCOMdKGiBiHUNZy1 ...
+#
+# Instead use this custom shell:
+# deps="base sdl2 vulkan vulkan-utils gl containers inline-c inline-c-cpp StateVar managed cabal-install OpenGL OpenGLRaw GLFW-b vector doctest"
+# nix-shell -p "haskellPackages.ghcWithPackages (p: with p; [${deps}])" -p libGL -p SDL2 -p glfw3 -p pkgconfig -p gcc
 { nixpkgs ? import <nixpkgs> { } }:
 let
   name = "hadertoy";
-  drv = nixpkgs.haskellPackages.callCabal2nix name ./. { };
-  shellDrv = nixpkgs.haskellPackages.shellFor {
-    withHoogle = true;
+  haskellPackages = nixpkgs.haskellPackages.override {
+    overrides = self: super: {
+      dear-imgui =
+        self.callCabal2nix "dear-imgui" ../../haskell-game/dear-imgui.hs { };
+    };
+  };
+  drv = haskellPackages.callCabal2nix name ./. { };
+  shellDrv = haskellPackages.shellFor {
+    withHoogle = false;
     packages = p: [ drv ];
-    buildInputs = with nixpkgs.haskellPackages; [ hlint cabal-install ];
+    buildInputs = with haskellPackages; [ hlint cabal-install ];
   };
 in if nixpkgs.lib.inNixShell then shellDrv else drv
